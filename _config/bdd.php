c6eaf422206d7b1513ae23fb806f8b2682b0a423
danoux10 <danybarbe10@gmail.com>
2022-03-18 13:56:32 +0100

    update sql

--- _config/bdd.php
+++ _config/bdd.php
@@ -1,6 +1,7 @@
 <?php
 	try {
 		$bdd = new PDO('mysql:host=localhost;dbname=rucher;charset=utf8', 'root', 'password');
+//  $bdd= new PDO('mysql:host=mysql-barbedany.alwaysdata.net;dbname=barbedany_ecf;charset=utf8', 'barbedany', 'Doudou10');
 	} catch (Exception $e) {
 		die('Erreur : ' . $e->getMessage());
 	}
